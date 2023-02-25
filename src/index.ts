62b766d6b73aafd4d0d0dc2bfe10855fafd5b3dc
Kieran Mann <kieran.mann@o8t.com>
2023-02-25 12:08:49 -0800

    encode/decode basic ExecOp

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,49 @@
-import { ExecOp } from './moby-buildkit/ops.js';
+import {
+  ExecOp,
+  NetMode,
+  ProxyEnv,
+  SecurityMode,
+} from "./moby-buildkit/ops.js";
+import protobuf from "protobufjs/minimal.js";
+import fs from "node:fs/promises";
 
-ExecOp.encode({
-    meta: { args: ['pwd'], env: [], cwd: '.'}
-} as any);
+const msgWriter = new protobuf.Writer();
+
+const proxyEnv: ProxyEnv = {
+  httpProxy: "",
+  httpsProxy: "",
+  ftpProxy: "",
+  noProxy: "",
+  allProxy: "",
+};
+
+const testExecOp = {
+  meta: {
+    args: ["pwd"],
+    env: [],
+    cwd: ".",
+    user: "root",
+    proxyEnv,
+    extraHosts: [],
+    hostname: "",
+    ulimit: [],
+    cgroupParent: "",
+    removeMountStubsRecursive: false,
+  },
+  mounts: [],
+  network: NetMode.UNSET,
+  security: SecurityMode.SANDBOX,
+  secretenv: [],
+};
+
+ExecOp.encode(testExecOp, msgWriter);
+
+const encoded = msgWriter.finish();
+
+await fs.writeFile("./llb-test.bin", encoded);
+
+console.log("Encoded msg", encoded);
+
+const readBack = ExecOp.decode(encoded);
+
+console.log("Decoded back to", readBack);
